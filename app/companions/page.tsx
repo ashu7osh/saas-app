e5dec1357c0edec719d54ab285abc45e537044bb
ashu7osh <ashutoshjatav19@gmail.com>
2025-07-03 12:14:21 +0530

    initial commit

---
+++ app/companions/page.tsx
@@ -0,0 +1,7 @@
+const CompanionsLibrary = () => {
+  return (
+    <div>Companion library</div>
+  )
+}
+
+export default CompanionsLibrary
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Button} from "@/components/ui/button"
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className='text-2xl underline'> welcome to my SaaS app</h1>
+      <Button>
+        let get started
+      </Button>
+    </div>
+  )
+}
+
+export default Page
